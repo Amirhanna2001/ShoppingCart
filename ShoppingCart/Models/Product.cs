ae18f1462b0ec57417ddb60bc806e59328fb2787
Amirhanna2001 <amirhanna111200@gmail.com>
2023-03-15 21:40:01 +0200

    Adding Products & Category Models

---
+++ ShoppingCart/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingCart.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a value")]
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
